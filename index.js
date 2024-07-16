a7fe1a5b228faa870f55c7fd293a5d089aaea34e
Dakota <dakota.klasky@gmail.com>
2024-07-16 13:51:26 -0400

    create functions

---
+++ index.js
@@ -0,0 +1,16 @@
+
+function receivesAFunction(callback){
+    return callback();
+}
+
+function returnsANamedFunction(){
+    return function named(){
+
+    }
+}
+
+function returnsAnAnonymousFunction(){
+    return function(){
+        
+    }
+}
